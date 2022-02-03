6d456c3a0e2a9d58656000ea778757901dbf8367
Thomas Browning <tb65536@uw.edu>
2022-02-02 22:56:39 -0800

    closed embedding

--- src/analysis/tb_fourier.lean
+++ src/analysis/tb_fourier.lean
@@ -9,21 +9,51 @@ import analysis.complex.circle
 
 section continuous_monoid_hom
 
-variables (A B C D E F : Type*)
+variables {A B C D E F : Type*}
   [monoid A] [monoid B] [monoid C] [monoid D] [comm_group E] [comm_group F]
   [topological_space A] [topological_space B] [topological_space C] [topological_space D]
   [topological_space E] [topological_group E] [topological_space F] [topological_group F]
 
 namespace continuous_monoid_hom
 
-lemma is_closed_embedding :
+lemma map_one (f : continuous_monoid_hom A B) : f 1 = 1 :=
+f.to_monoid_hom.map_one
+
+lemma map_mul (f : continuous_monoid_hom A B) (a b : A) : f (a * b) = f a * f b :=
+f.to_monoid_hom.map_mul a b
+
+open_locale pointwise
+
+lemma is_closed_embedding [has_continuous_mul B] [t2_space B] :
   closed_embedding (to_continuous_map : continuous_monoid_hom A B → C(A, B)) :=
-⟨is_embedding A B, begin
-  -- complement is covered by `{f(x)=a,f(y)=b,f(z)=c}` for fixed `x * y = z` and `a * b ≠ c`.
-  -- Now find open neighborhoods `U,V,W` of `a,b,c` with `U * V ∩ W = ∅`.
-  -- Then use compact open topology to finish.
-  sorry
-end⟩
+⟨is_embedding A B, ⟨begin
+  suffices : (set.range (to_continuous_map : continuous_monoid_hom A B → C(A, B))) =
+    ({f | f '' {1} ⊆ {1}ᶜ} ∪ ⋃ (x y) (U V W) (hU : is_open U) (hV : is_open V) (hW : is_open W)
+    (h : disjoint (U * V) W), {f | f '' {x} ⊆ U} ∩ {f | f '' {y} ⊆ V} ∩ {f | f '' {x * y} ⊆ W})ᶜ,
+  { rw [this, compl_compl],
+    refine (continuous_map.is_open_gen is_compact_singleton is_open_compl_singleton).union _,
+    repeat { apply is_open_Union, intro, },
+    repeat { apply is_open.inter },
+    all_goals { apply continuous_map.is_open_gen is_compact_singleton, assumption } },
+  simp_rw [set.compl_union, set.compl_Union, set.image_singleton, set.singleton_subset_iff,
+    set.ext_iff, set.mem_inter_iff, set.mem_Inter, set.mem_compl_iff],
+  refine λ f, ⟨_, _⟩,
+  { rintros ⟨f, rfl⟩,
+    exact ⟨λ h, h f.map_one, λ x y U V W hU hV hW h ⟨⟨hfU, hfV⟩, hfW⟩,
+      h ⟨set.mul_mem_mul hfU hfV, (congr_arg (∈ W) (f.map_mul x y)).mp hfW⟩⟩ },
+  { rintros ⟨hf1, hf2⟩,
+    suffices : ∀ x y, f (x * y) = f x * f y,
+    { exact ⟨continuous_monoid_hom.mk f (of_not_not hf1) this, continuous_map.ext (λ _, rfl)⟩ },
+    intros x y,
+    specialize hf2 x y,
+    contrapose! hf2,
+    obtain ⟨UV, W, hUV, hW, hfUV, hfW, h⟩ := t2_separation hf2.symm,
+    obtain ⟨U, V, hU, hV, hfU, hfV, h'⟩ :=
+    is_open_prod_iff.mp (hUV.preimage (@continuous_mul B _ _ _)) (f x) (f y) hfUV,
+    refine ⟨U, V, W, hU, hV, hW, ((disjoint_iff.mpr h).mono_left _), ⟨hfU, hfV⟩, hfW⟩,
+    rintros _ ⟨x, y, hx : (x, y).1 ∈ U, hy : (x, y).2 ∈ V, rfl⟩,
+    exact h' ⟨hx, hy⟩ },
+end⟩⟩
 
 -- https://math.stackexchange.com/questions/1502405/why-is-the-pontraygin-dual-a-locally-compact-group
 -- https://math.stackexchange.com/questions/2622521/pontryagin-dual-group-inherits-local-compactness
@@ -38,9 +68,6 @@ end
 
 def eval : continuous_monoid_hom A (continuous_monoid_hom (continuous_monoid_hom A E) E) :=
 swap_args (id (continuous_monoid_hom A E))
-
-#check monoid_hom.comp_hom
-#check monoid_hom.comp_hom'
 
 /-def covariant : continuous_monoid_hom (continuous_monoid_hom E F)
   (continuous_monoid_hom (continuous_monoid_hom A E) (continuous_monoid_hom A F)) :=
